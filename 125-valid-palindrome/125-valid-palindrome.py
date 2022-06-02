7f3e954c95f6ac87396b79e5b204c2d6a2338e63
ashishlac <104764050+ashishlac@users.noreply.github.com>
2022-06-02 20:48:00 +0530

    Time: 28 ms (97.12%), Space: 13.9 MB (96.27%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -8,14 +8,13 @@ class Solution(object):
         l=len(s)
         left=0
         right=l-1
-        while left<=right:
-            while left < l and not s[left].isalnum():
+        while left<right:
+            while left < right and not s[left].isalnum():
                 left=left+1
-            while right >= 0 and not s[right].isalnum():
+            while left < right and not s[right].isalnum():
                 right= right-1
-            
-            if left < len and right >=0 and left<=right and s[left].lower() != s[right].lower() :
-                return 0
+            if s[left].lower() != s[right].lower() :
+                return False
             left=left+1
             right=right-1
-        return 1
+        return True
