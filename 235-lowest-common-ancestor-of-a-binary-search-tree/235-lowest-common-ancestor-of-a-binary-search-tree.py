6eba77298ac73eb7a9d624a2b69fe2c3b71bc8aa
ashishlac <104764050+ashishlac@users.noreply.github.com>
2022-06-04 23:44:33 +0530

    Time: 121 ms (10.16%), Space: 21.2 MB (92.53%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -13,36 +13,7 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        curr=root
-        parent=root
-        while curr is not None:
-            if curr.val == "null":
-                return parent
-            if curr.val == p.val or curr.val == q.val:
-                return curr
-            if p.val > curr.val:
-                p_right=1
-            elif p.val < curr.val:
-                p_right= -1
-            else:
-                p_right = 0
-            if q.val > curr.val:
-                q_right=1
-            elif q.val < curr.val:
-                q_right= -1
-            else:
-                q_right = 0
-            if p_right != q_right:
-                return parent
-            if p_right == 0:
-                return p
-            elif q_right == 0:
-                return q
-            if p_right > 0 and q_right > 0:
-                curr=curr.right
-            else:
-                curr=curr.left
+        while (root.val - p.val) * (root.val - q.val) > 0:
+            root = (root.left, root.right)[p.val > root.val]
+        return root
             
-            parent=curr
-            
-        return 0
