d853caedfa7cf765c6ff6415956b75325276f2ef
ashishlac <104764050+ashishlac@users.noreply.github.com>
2022-06-16 00:11:11 +0530

    Time: 1680 ms (27.04%), Space: 49.7 MB (96.74%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -9,27 +9,23 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        fast = head
-        string=[]
-        count = 1
+        # rev records the first half, need to set the same structure as fast, slow, hence later we have rev.next
+        rev = None
+        # initially slow and fast are the same, starting from head
+        slow = fast = head
         while fast and fast.next:
-            string.append(head.val)
-            head = head.next
-            fast = fast.next
-            count += 1
-            if not fast:
-                break
-            fast = fast.next
-            if not fast:
-                break
-            count += 1
-        if count % 2 == 1:
-            head = head.next
-                    
-        while head:
-            if head.val != string.pop():
-                return False
-            head = head.next
-            
-            
-        return True
+            # fast traverses faster and moves to the end of the list if the length is odd
+            fast = fast.next.next
+
+            # take it as a tuple being assigned (rev, rev.next, slow) = (slow, rev, slow.next), hence the re-assignment of slow would not affect rev (rev = slow)
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+           # fast is at the end, move slow one step further for comparison(cross middle one)
+            slow = slow.next
+        # compare the reversed first half with the second half
+        while rev and rev.val == slow.val:
+            slow = slow.next
+            rev = rev.next
+
+        # if equivalent then rev become None, return True; otherwise return False 
+        return not rev
