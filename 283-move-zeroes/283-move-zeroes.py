a753a5b846b16475986617d62c1ad632985ddc56
ashishlac <104764050+ashishlac@users.noreply.github.com>
2022-06-16 19:48:01 +0530

    Time: 215 ms (45.44%), Space: 14.6 MB (82.76%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,19 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        l = len(nums) -1
-        self.counter=0
         i=0
-        while i <= l:
-            if nums[i] == 0:
-                nums.pop(i)
-                self.counter += 1
-                l -= 1
-            else:
+        for j in range(0,len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
                 i += 1
-               
-        while self.counter > 0 :
-            nums.append(0)
-            self.counter -= 1
         return nums
         
